c4dee840805173480b2afdaf408ceade7e0c6fd1
User <d1relka_pix@mail.ru>
2024-09-24 15:03:38 +0300

    Добавлены геттеры для полей классов Hero и NyHero, корректированны поля класса MyHero

--- heroClasses/heroesClasses.py
+++ heroClasses/heroesClasses.py
@@ -11,6 +11,15 @@ class Hero:
         self.__level = 0
         self.__exp = 0
 
+    def get_name(self):
+        return self.__name
+    def get_MyHeroSkills(self):
+        return self.__my_hero_skills
+    def get_level(self):
+        return self.__level
+    def get_exp(self):
+        return self.__exp
+
     def get_skills(self, character_class):
         """Добавлен геттер для навыков, результат зависит от выбранного класса"""
         if character_class == 'воин':
@@ -23,31 +32,33 @@ class Hero:
             exit("Ошибка перезапустите программу!")
 
     def get_new_level(self):
-        if self.exp >= 1000:
-            self.level = 3
+        if self.__exp >= 1000:
+            self.__level = 3
             self.add_skill()
-        elif self.exp >= 500:
-            self.level = 2
+        elif self.__exp >= 500:
+            self.__level = 2
             self.add_skill()
-        elif self.exp >= 200:
-            self.level = 1
+        elif self.__exp >= 200:
+            self.__level = 1
             self.add_skill()
         else:
-            self.level = 0
+            self.__level = 0
         return f"Герой {self.name}, теперь {self.level} уровня, навыки: {', '.join(self.my_hero_skills)}"
 
     def add_exp(self, exp):
-        self.exp += exp
+        self.__exp += exp
         new_level = self.get_new_level()
         return new_level
 
     def add_skills(self):
-        pass
+
 
 
 class MyHero(Hero):
 
-    def __init__(self, class):
-        self.__class = class
+    def __init__(self, character_class):
+        self.__character_class = character_class
 
+    def get_class(self):
+        return self.__character_class
 
